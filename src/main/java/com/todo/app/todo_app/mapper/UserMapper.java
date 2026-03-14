5f248a687d71d27b84b52ad688797d8aae94f88d
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-14 18:45:37 +0300

    update user mapper

--- src/main/java/com/todo/app/todo_app/mapper/UserMapper.java
+++ src/main/java/com/todo/app/todo_app/mapper/UserMapper.java
@@ -4,11 +4,13 @@ import com.todo.app.todo_app.dto.request.SaveUserDto;
 import com.todo.app.todo_app.dto.UserDetailsImpl;
 import com.todo.app.todo_app.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
+    @Mapping(source = "email", target = "email")
     User toUser(SaveUserDto userDto);
 
     UserDetailsImpl toUserDetails(User user);
