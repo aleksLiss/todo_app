29ba18b2b287e384490692f5e00f34cb45845e01
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-14 18:50:08 +0300

    update TaskDto

--- src/main/java/com/todo/app/todo_app/dto/TaskDto.java
+++ src/main/java/com/todo/app/todo_app/dto/TaskDto.java
@@ -1,4 +1,12 @@
 package com.todo.app.todo_app.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
 public class TaskDto {
+    @NotBlank(message = "Title must be not empty")
+    @Length(min = 1, max = 100, message = "Title mu be length from 1 to 100")
+    private String title;
 }
