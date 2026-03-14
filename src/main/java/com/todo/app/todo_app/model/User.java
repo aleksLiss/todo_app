18577f670f9879ee29580c0203cffde655119663
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-14 18:45:18 +0300

    update model user

--- src/main/java/com/todo/app/todo_app/model/User.java
+++ src/main/java/com/todo/app/todo_app/model/User.java
@@ -9,7 +9,7 @@ import org.hibernate.validator.constraints.Length;
 import java.util.UUID;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Data
 public class User {
 
@@ -21,7 +21,7 @@ public class User {
     @Column(name = "email", unique = true, nullable = false)
     private String email;
     @NotBlank(message = "Password must be not empty")
-    @Length(min = 8, max = 20, message = "Length must be from 8 to 20 symbols")
+    @Length(min = 8, max = 100, message = "Length must be from 8 to 20 symbols")
     @Column(name = "password", nullable = false)
     private String password;
 }
