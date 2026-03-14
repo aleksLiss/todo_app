bcd8187420ef6af056a97244f983de60025cc076
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-14 18:46:37 +0300

    update SaveUserDto

--- src/main/java/com/todo/app/todo_app/dto/request/SaveUserDto.java
+++ src/main/java/com/todo/app/todo_app/dto/request/SaveUserDto.java
@@ -2,16 +2,20 @@ package com.todo.app.todo_app.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaveUserDto {
 
     @NotBlank(message = "Email must be not empty")
     @Email(message = "Email must be equals like 'max@google.com'")
     private String email;
     @NotBlank(message = "Password must be not empty")
-    @Length(min = 8, max = 20, message = "Password length must be from 8 to 20 symbols")
+    @Length(min = 8, max = 100, message = "Password length must be from 8 to 20 symbols")
     private String password;
 }
