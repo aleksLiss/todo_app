829245866774258e25233db759df98dda0c546ce
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-18 00:17:56 +0300

    add user dto

---
+++ backend/src/main/java/com/todo/app/backend/dto/ExceptionDto.java
@@ -0,0 +1,6 @@
+package com.todo.app.backend.dto;
+
+public record ExceptionDto(
+        String message
+) {
+}
---
+++ backend/src/main/java/com/todo/app/backend/dto/RequestUserDto.java
@@ -0,0 +1,16 @@
+package com.todo.app.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record RequestUserDto(
+        @NotBlank(message = "Email must be not empty")
+        @Length(message = "Email length must be from 3 to 30", min = 3, max = 30)
+        @Email(message = "Email must be like 'max@google.com'")
+        String email,
+        @NotBlank(message = "Password must be not empty")
+        @Length(message = "Password length must be from 8 to 100", min = 8, max = 100)
+        String password
+) {
+}
