96b99fbb0fffda1c99d800f13bfce639e0e82f0a
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-21 13:12:54 +0300

    create UserPrincipal

---
+++ backend/src/main/java/com/todo/app/backend/security/UserPrincipal.java
@@ -0,0 +1,31 @@
+package com.todo.app.backend.security;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public record UserPrincipal(
+        UUID id,
+        String email,
+        String password
+) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
