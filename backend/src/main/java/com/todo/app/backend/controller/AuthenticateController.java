7075669b8a94ab885299306e010062c5425119aa
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-24 22:34:43 +0300

    update AuthenticateController

--- backend/src/main/java/com/todo/app/backend/controller/AuthenticateController.java
+++ backend/src/main/java/com/todo/app/backend/controller/AuthenticateController.java
@@ -1,20 +1,18 @@
 package com.todo.app.backend.controller;
 
-import com.todo.app.backend.dto.JwtTokenResponseDto;
-import com.todo.app.backend.dto.RequestUserDto;
+import com.todo.app.backend.dto.LoginUserDto;
 import com.todo.app.backend.security.UserPrincipal;
 import com.todo.app.backend.service.JwtService;
 import com.todo.app.backend.service.UserService;
-import jakarta.validation.Valid;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -24,10 +22,12 @@ public class AuthenticateController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<@NonNull JwtTokenResponseDto> loginUser(@Valid @ModelAttribute RequestUserDto requestUserDto) {
-        UserPrincipal userPrincipal = userService.getUserByEmail(requestUserDto.email());
+    public ResponseEntity<?> loginUser(@RequestBody LoginUserDto loginUserDto) {
+        UserPrincipal userPrincipal = userService.getUserByEmail(loginUserDto.email());
         String token = jwtService.generateToken(userPrincipal);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new JwtTokenResponseDto(token));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .build();
     }
 }
