b2d4fd132ecb99a8e0c7d28399596dbd5021831a
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-24 22:35:35 +0300

    add sign in dto

--- backend/src/main/java/com/todo/app/backend/dto/LoginUserDto.java
+++ backend/src/main/java/com/todo/app/backend/dto/LoginUserDto.java
@@ -4,9 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
-public record RequestUserDto(
+public record LoginUserDto(
         @NotBlank(message = "Email must be not empty")
-        @Length(message = "Email length must be from 3 to 30", min = 3, max = 30)
+        @Length(message = "Email length must be from 5 to 50", min = 5, max = 50)
         @Email(message = "Email must be like 'max@google.com'")
         String email,
         @NotBlank(message = "Password must be not empty")
