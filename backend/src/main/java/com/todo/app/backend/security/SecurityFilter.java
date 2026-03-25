dbff97e642d60c772c683d3f911f0a4f72842fe5
aleksLiss <darklissik.darklissik@gmail.com>
2026-03-25 23:12:34 +0300

    update SecurityFilter

--- backend/src/main/java/com/todo/app/backend/security/SecurityFilter.java
+++ backend/src/main/java/com/todo/app/backend/security/SecurityFilter.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,12 +27,14 @@ public class SecurityFilter {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/user").permitAll()
                         .requestMatchers("/static/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/user").authenticated())
+                        .requestMatchers(HttpMethod.GET, "/api/user").authenticated()
+                        .requestMatchers("/api/tasks/**").authenticated())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
