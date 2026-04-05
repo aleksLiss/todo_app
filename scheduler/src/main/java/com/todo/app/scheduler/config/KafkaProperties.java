2263e1ab8fcf5cd47da8f90bf7efd6ca3f13f976
aleksLiss <darklissik.darklissik@gmail.com>
2026-04-05 15:59:33 +0300

    add config files

---
+++ scheduler/src/main/java/com/todo/app/scheduler/config/KafkaProperties.java
@@ -0,0 +1,18 @@
+package com.todo.app.scheduler.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "spring.kafka")
+public record KafkaProperties(
+        String bootstrapServers,
+        Producer producer,
+        String topicName,
+        String partitionsCount,
+        String replicationCount
+) {
+    public record Producer(
+            String keySerializer,
+            String valueSerializer
+    ) {
+    }
+}
