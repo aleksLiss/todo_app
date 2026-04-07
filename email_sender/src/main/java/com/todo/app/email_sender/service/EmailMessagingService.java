45823b4643c9bf93187d812f4aa2b4f372f744a9
aleksLiss <darklissik.darklissik@gmail.com>
2026-04-07 23:13:13 +0300

    update service

--- email_sender/src/main/java/com/todo/app/email_sender/service/EmailMessagingService.java
+++ email_sender/src/main/java/com/todo/app/email_sender/service/EmailMessagingService.java
@@ -1,13 +1,22 @@
 package com.todo.app.email_sender.service;
 
+import com.todo.app.email_sender.dto.KafkaMessageDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class EmailMessagingService {
 
+    private final JavaMailSender javaMailSender;
 
+    public void sendEmail(String from, String to, String message) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(from);
+        simpleMailMessage.setSubject(message);
+        simpleMailMessage.setTo(to);
+        javaMailSender.send(simpleMailMessage);
+    }
 }
--- email_sender/src/main/java/com/todo/app/email_sender/service/KafkaConsumerService.java
+++ email_sender/src/main/java/com/todo/app/email_sender/service/KafkaConsumerService.java
@@ -1,17 +1,29 @@
 package com.todo.app.email_sender.service;
 
+import com.google.gson.Gson;
+import com.todo.app.email_sender.config.SmtpProperties;
+import com.todo.app.email_sender.dto.KafkaMessageDto;
+import com.todo.app.email_sender.mapper.JsonMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class KafkaConsumerService {
+
+    private final JsonMapper jsonMapper;
+    private final EmailMessagingService emailMessagingService;
+    private final Gson gson;
+    private final SmtpProperties smtpProperties;
 
     @KafkaListener(topics = "${KAFKA_TOPIC_NAME}", groupId = "${KAFKA_GROUP_ID}")
     public void receiveMessage(String message){
-        log.info("Сообщение из Kafka получено автоматически: {}", message);
+        KafkaMessageDto messageDto = jsonMapper.fromJson(gson, message);
+        emailMessagingService.sendEmail(
+                smtpProperties.from(),
+                messageDto.email(),
+                messageDto.message()
+        );
     }
 }
