7968b7eaae5b3222f40bfc0fa6532f2d8cee870b
aleksLiss <darklissik.darklissik@gmail.com>
2026-04-07 23:13:26 +0300

    update main class

--- email_sender/src/main/java/com/todo/app/email_sender/EmailSenderApplication.java
+++ email_sender/src/main/java/com/todo/app/email_sender/EmailSenderApplication.java
@@ -1,12 +1,13 @@
 package com.todo.app.email_sender;
 
 import com.todo.app.email_sender.config.KafkaProperties;
+import com.todo.app.email_sender.config.SmtpProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(KafkaProperties.class)
+@EnableConfigurationProperties({KafkaProperties.class, SmtpProperties.class})
 public class EmailSenderApplication {
 
     public static void main(String[] args) {
