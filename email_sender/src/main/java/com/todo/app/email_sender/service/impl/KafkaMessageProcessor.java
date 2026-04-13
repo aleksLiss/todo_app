39f5fd034a805c40ce93c308faa57fccc7bd3046
aleksLiss <darklissik.darklissik@gmail.com>
2026-04-13 23:19:59 +0300

    update service into email_sender

--- email_sender/src/main/java/com/todo/app/email_sender/service/impl/KafkaMessageProcessor.java
+++ email_sender/src/main/java/com/todo/app/email_sender/service/impl/KafkaMessageProcessor.java
@@ -5,10 +5,12 @@ import com.todo.app.email_sender.service.CreatorService;
 import com.todo.app.email_sender.service.MessageProcessor;
 import com.todo.app.email_sender.service.SenderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaMessageProcessor implements MessageProcessor {
@@ -19,7 +21,11 @@ public class KafkaMessageProcessor implements MessageProcessor {
     @EventListener
     @Override
     public void processMessage(MessageReceivedEvent event) {
-        SimpleMailMessage message = creatorService.create(event.dto());
-        senderService.sendEmail(message);
+        SimpleMailMessage message = receiveMessage(event);
+//        senderService.sendEmail(message);
+    }
+
+    private SimpleMailMessage receiveMessage(MessageReceivedEvent event) {
+        return creatorService.create(event.dto());
     }
 }
