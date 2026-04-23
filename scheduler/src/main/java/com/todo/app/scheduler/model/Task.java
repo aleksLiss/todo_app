fca2c3c5bd515544aed6ef01447c670745a95274
aleksLiss <darklissik.darklissik@gmail.com>
2026-04-23 20:45:28 +0300

    update model

--- scheduler/src/main/java/com/todo/app/scheduler/model/Task.java
+++ scheduler/src/main/java/com/todo/app/scheduler/model/Task.java
@@ -34,8 +34,9 @@ public class Task {
 
     @PrePersist
     protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-        this.isCompleted = false;
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
     }
 
     public Task() {
